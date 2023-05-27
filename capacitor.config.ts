dfda86afc23275af4866ef84f01da7568eaee71c
GustavoSoriani <gustavo.sorianig@gmail.com>
2023-05-27 15:28:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meu-portfolio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
